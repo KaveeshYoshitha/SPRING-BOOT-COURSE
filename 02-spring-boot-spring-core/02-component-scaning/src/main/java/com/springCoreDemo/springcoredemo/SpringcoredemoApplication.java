5ba8d7343c8ee4b305442fa19f38b1545ab3e906
Kaveesh Yoshitha <yoshithakaveesh83@gmail.com>
2024-09-11 00:25:17 +0530

    02-component-scaning Added

---
+++ 02-spring-boot-spring-core/02-component-scaning/src/main/java/com/springCoreDemo/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,16 @@
+package com.springCoreDemo.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication(
+//		scanBasePackages = {"com.springCoreDemo","com.springCoreDemo.util"}
+//)
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
